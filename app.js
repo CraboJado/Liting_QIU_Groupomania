bc80883323f3e1777e340ba1b720e1e9b7bd73bc
lq <liting_qiu@hotmail.com>
2022-07-22 15:04:53 +0200

    add middleware for static files

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post')
 const errorHandler = require('./middlewares/errorHandler');
+
 
 // body parser
 app.use(express.json());
 
 // middleware for static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname + '\\public');
+console.log(path.join(__dirname, 'public'));
 
 // user routes
 app.use('/api/auth',userRoutes);
