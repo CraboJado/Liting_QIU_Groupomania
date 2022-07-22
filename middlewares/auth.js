edba1e15787d8139a4d924bc8fd67d3632603944
lq <liting_qiu@hotmail.com>
2022-07-22 18:17:37 +0200

    add auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const ErrorResponse = require('../utils/errorResponse');
+
+const auth = (req, res, next) => {
+    console.log('in auth controller');
+    console.log('req.headers.authorization',req.headers.authorization);
+    console.log('req.params.userId',req.params.userId);
+
+
+    let token
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        token = req.headers.authorization.split(' ')[1]
+    }
+
+    if(!token){
+        return next( new ErrorResponse('unauthorized request, missing valid credentials',401))
+    }
+
+    const decodedToken = jwt.verify(token,process.env.TOKEN_KEY);
+    const { data: userId, isAdmin } = decodedToken;
+    req.auth = { userId, isAdmin };
+
+    if( userId !== + req.params.userId){
+       return next( new ErrorResponse('unauthorized request, invalid credentials',401))
+    }
+
+    next();
+} 
+
+module.exports = auth;
