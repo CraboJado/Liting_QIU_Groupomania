0529d14c43739099ea389593bc3eb74b1b9b662e
lq <liting_qiu@hotmail.com>
2022-07-27 16:49:53 +0200

    modify signup controller by adding isAdmin role for users

--- controllers/user.js
+++ controllers/user.js
@@ -1,40 +1,42 @@
 const mysqlConnect = require('../config/db');
 const bcrypt = require ('bcrypt');
 const jwt = require('jsonwebtoken');
-const ErrorResponse = require('../utils/errorResponse')
+const ErrorResponse = require('../utils/errorResponse');
+const getMysqlDate = require('../utils/getMysqlDate');
+const { v4: uuidv4 } = require('uuid');
 
 exports.signup = (req,res,next) => {
     console.log('in signup controller', req.body);
     const { email, password, name, department_id, job_id } = req.body;
-    const users_query = `INSERT INTO users (email, password, name, department_id, job_id, create_time) VALUES(?,?,?,?,?,?)`;
-  
-    const createDate = new Date(Date.now());
-    let mysqlDate = `${createDate.getFullYear()}-${createDate.getMonth()+1}-${createDate.getDate()} ${createDate.getHours()}:${createDate.getMinutes()}:${createDate.getSeconds()}`;
-
+    // signup a superadmin with a fixied email, 
+    // this account can set other users as subAdmin with isAdmin = 1 if we need more admin accounts
+    let isAdmin;
+    email === process.env.SUPERADMIN ? isAdmin = 2 : isAdmin = 0;
+    
     mysqlConnect.then( connection => {
         bcrypt.hash(password,10)
         .then( hash => {
-            connection.query(users_query, [email, hash, name, department_id, job_id, mysqlDate], (error, results, fields) => {
+            const users_query = `INSERT INTO users (user_id, email, password, name, department_id, job_id, isAdmin, create_time) VALUES(?,?,?,?,?,?,?,?)`;
+            const insert_values = [uuidv4(),email, hash, name, department_id, job_id, isAdmin, getMysqlDate()]; // ajouter userid
+
+            connection.query(users_query, insert_values, (error, results, fields) => {
                 if (error) {
                     return next(error)
                 }
-                console.log('insert to users results',results);
-                res.status(201).json({ message:"Successfully Registered" })
+                res.status(201).json({ message:"utilisateur créé" })
             })
-
         })
         .catch( error => {
             console.log('error in hash', error);
-            return next(error)
+            next(error)
         })
-    
     })
 }
 
 exports.login = (req,res,next) => {
     console.log('in login controller', req.body);
     const { email, password } = req.body;
-    console.log(email);
+
     const users_query = `SELECT * FROM users WHERE email = ?`;
     mysqlConnect.then( connection => {
         connection.query(users_query,[email], (error, results, fields) => {
@@ -44,15 +46,15 @@ exports.login = (req,res,next) => {
             }
 
             if(results.length === 0){
-               return next( new ErrorResponse('user not exists', 404) )
+               return next( new ErrorResponse('utilisateur n\'existe pas', 404) )
             }
 
             bcrypt.compare(password,results[0].password)
             .then( valid => {
                 if(!valid) {
-                    return next( new ErrorResponse('unauthorized request', 401) )
+                    return next( new ErrorResponse('requête non autorisée', 401) )
                 }
-                // if admin , send token with admin true
+                // if admin , send token with admin true ( plusieur admin ? ajoute isAdmin dans table user)
                 if(results[0].email === process.env.ADMIN){
                     const token = jwt.sign({ data: results[0].user_id, isAdmin:true }, process.env.TOKEN_KEY, { expiresIn: process.env.TOKEN_EXPIRE });
                     res.status(200).json({ userId:results[0].user_id, token })
