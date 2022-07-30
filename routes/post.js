4c735de5b1cff45886bac63ca5f16ad8980dd352
lq <liting_qiu@hotmail.com>
2022-07-30 17:20:38 +0200

    add like post routes

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {addPost, modifyPost, deletePost, getAllPosts, getPost} = require('../controllers/post');
+const {addPost, modifyPost, deletePost, getAllPosts, getPost , likePost} = require('../controllers/post');
 const multer = require('../middlewares/multer_config');
 const auth = require('../middlewares/auth');
 
 router.get('/:userId', auth, getAllPosts)
 
-router.get('/:id/:userId', getPost)
+router.get('/:id/:userId', auth,getPost)
 
 router.post('/:userId', auth, multer, addPost)
 
@@ -14,9 +14,7 @@ router.put('/:id/:userId',auth, multer, modifyPost)
 
 router.delete('/:id/:userId',auth, deletePost)
 
-router.post('/:id/:userId/like',(req,res,next)=>{
-    res.status(201).json({message : 'like or dislike post route'})
-})
+router.post('/:id/:userId/like',auth,likePost)
 
 router.post('/:id/:userId/comment',(req,res,next)=>{
     res.status(201).json({message : 'comment post route'})
