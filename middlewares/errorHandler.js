50912c57b721d16e9abf0fbaa5ef089659834039
lq <liting_qiu@hotmail.com>
2022-08-01 15:32:34 +0200

    modify

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,12 +4,14 @@ const errorHandler = (err, req, res, next) => {
     console.log('in error middleware')
     let error = {...err};
     error.message = err.message 
-    
+    console.log(error);
+
+    // check mysql error Only populated from MySQL server error.
     if(error.errno) {
         error = new ErrorResponse(error.sqlMessage,400);
     }
 
-    res.status(error.statusCode || 500).json({ errorInMiddle: error.message || "Server Error"})
+    res.status(error.statusCode || 500).json({ errorInErrorHandler: error.message || "Server Error"})
 }
 
 module.exports = errorHandler;
