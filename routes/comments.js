1291865e30867497d38dc7afa271454c0c904b6e
lq <liting_qiu@hotmail.com>
2022-08-01 22:18:34 +0200

    add like comment route

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('../middlewares/multer_config');
 const auth = require('../middlewares/auth')
-const { addComment, modifyComment, deleteComment, getAllComments} = require('../controllers/comments')
+const { addComment, modifyComment, deleteComment, getAllComments, likeComment} = require('../controllers/comments')
 
 router.get('/:userId/:postId', auth, getAllComments)
 
@@ -12,9 +12,7 @@ router.put('/:id/:userId',auth,multer,modifyComment)
 
 router.delete('/:id/:userId',auth, deleteComment)
 
-router.post('/:userId/like',(req,res,next) => {
-    res.status(200).json({ message: 'like one comment'})
-})
+router.post('/:id/:userId/like', auth, likeComment)
 
 router.post('/:userId/:id/reply',(req,res,next) => {
     res.status(200).json({ message: 'reply at one comment'})
