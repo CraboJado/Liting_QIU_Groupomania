062f63d9bcca59e957401adca418e497cdba736d
lq <liting_qiu@hotmail.com>
2022-08-09 16:15:47 +0200

    creat folder client for creat-react-app and install sass, fontawsome libs and link google font in index.html for styles

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars,faUser } from '@fortawesome/free-solid-svg-icons'
+import'./app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      ABC
+          <FontAwesomeIcon icon={faBars} />
+          <FontAwesomeIcon icon={faUser} />
+    </div>
+  );
+}
+
+export default App;
