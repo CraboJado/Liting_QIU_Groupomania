6bd6cab387ed910d6b2790a6cdd860f596299a48
lq <liting_qiu@hotmail.com>
2022-08-12 16:45:35 +0200

    Add footer component and styles

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <h1>Goupomania</h1>
+            <ul>
+                <li>A propos</li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;
+
