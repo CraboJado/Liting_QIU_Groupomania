f3d4367ada0ca6b34e4e66145d958caf1485ba26
lq <liting_qiu@hotmail.com>
2022-08-12 16:51:04 +0200

    modify styles add classname btn to h3

--- client/src/components/LandingCenter/LandingCenter.jsx
+++ client/src/components/LandingCenter/LandingCenter.jsx
@@ -4,15 +4,15 @@ import './landingCenter.scss'
 
 export default function LandingCenter() {
   return (
-    <div className='LandingCenter'>
+    <div className='landingCenter'>
         <h2>
-            Bienvenue à votre communauté de Groupomania <br/>
+            Bienvenue à votre communauté<br/>
             S'exprimez, Partagez , Rigolez ensemble
         </h2>
         <img src="./logos/icon-left-font-monochrome-black.png" alt="" />
-        <h2>
-            <Link to = '/signup' className='btn'>Rejoignez vos collègues</Link>
-        </h2>
+        <h3 className='btn'>
+            <Link to = '/signup' >Rejoignez vos collègues</Link>
+        </h3>
     </div>
   )
 }
