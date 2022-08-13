7214efdf9270e921e6a8dbd30ef180f77698774c
lq <liting_qiu@hotmail.com>
2022-08-13 15:37:19 +0200

    landing page done

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
-import LandingCenter from '../../components/LandingCenter/LandingCenter';
-import Footer from '../../components/Footer/Footer';
+import React  ,{useState} from 'react';
+import Header from '../../components/header/Header';
+import LandingCenter from '../../components/landingCenter/LandingCenter';
+import Footer from '../../components/footer/Footer';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './landing.scss';
 
+export default function Landing() {
+  const [isShow, setIsShow] = useState(false);
 
-
-export default function landing() {
+  const clickHandler = () => {
+      setIsShow(!isShow)
+  }
   return (
     <div className='landing'>
-        <Header/>
+        <Header>
+          <FontAwesomeIcon icon={faBars} onClick = {clickHandler}/>
+          {isShow && 
+            <div className='dropDown'>
+              <Link to = '/signup' className='btn'>signup</Link>
+              <Link to = '/login'  className='btn' >login</Link>
+            </div>
+          }
+        </Header>
         <LandingCenter/>
         <Footer/>
     </div>
