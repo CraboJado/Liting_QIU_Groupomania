c1ea4297c4e671976cf3329e93ef62205477bc8a
lq <liting_qiu@hotmail.com>
2022-09-02 16:18:06 +0200

    add logic to send customized error message for error from  JSONWEBTOKEN

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,20 +1,20 @@
-const ErrorResponse = require('../utils/errorResponse');
+const { ErrorResponse } = require('../utils/utils');
 
 const errorHandler = (err, req, res, next) => {
     console.log('in error middleware')
+    // copy err obj then assign to error Obj
     let error = {...err};
     error.message = err.message 
     console.log(error);
 
-    // check mysql error Only populated from MySQL server error.
-    // code: 'ER_DUP_ENTRY',(Duplicate entry)
-    // errno: 1062,
-    // errno: 3819, Check constraint 'users_chk_1' is violate
-    // errno: 1048, "Column 'name' cannot be null",
-    // 
-
+    // if error is populated from MySQL server error, response MySQL server error symbol (error.code)
     if(error.errno) {
         error = new ErrorResponse(error.code,400);
+    }
+
+    // if error is from JsonWebToken, customize error response
+    if(error.name === 'JsonWebTokenError' || 'TokenExpiredError') {
+        error = new ErrorResponse(error.message,400);
     }
 
     res.status(error.statusCode || 500).json({ errorMsg: error.message || "Server Error"})
