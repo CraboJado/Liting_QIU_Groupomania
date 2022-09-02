1d8379507fb571e9f62034b5afd062f2ec706a9f
lq <liting_qiu@hotmail.com>
2022-09-02 16:16:16 +0200

    add DB_PORT varibale env.

--- server/config/db.js
+++ server/config/db.js
@@ -1,24 +1,27 @@
 const mysql = require('mysql2');
-const ErrorResponse = require('../utils/errorResponse');
 
 const dbConfig = {
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
 	database:process.env.DB_DATABASE,
+    port:process.env.DB_PORT,
     multipleStatements: true
 }
 
 const mysqlConnect = (config) => {
     return new Promise((resolve, reject) => {
+        // create connection obj for mysql DB
         const connection = mysql.createConnection(config);
+        
+        // connect with mysql DB, return out connection obj when connected successfully
         connection.connect( (err) => {
             if(err) {
                 reject(err);
                 console.log( err + 'connection au mysql échoué');
             }else {
                 resolve(connection); 
-				console.log('connection au mysql réussie');
+				console.log('connection au mysql réussie'); 
             }
         })
     })
