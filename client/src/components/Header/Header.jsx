83539f01d5df9c2fb1efca309c051bb4c132046b
lq <liting_qiu@hotmail.com>
2022-09-27 14:59:15 +0200

    modify Header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './header.scss'
+import React  from 'react';
+import './header.scss';
 
 export default function Header(props) {
+
   return (
     <header className='header'>
-        <img src ={"./logos/icon-left-font-monochrome-white.svg"} alt="Groupomania logo"/>
-        <nav className='headerNav'>
-            {props.children}
-        </nav>
+      <h1 className="header__logo">
+        <img className="header__img" src ={"./logos/icon-left-font-monochrome-white.svg"} alt="Groupomania logo"/>
+      </h1>
+        { props.children }
     </header>
   )
 }
