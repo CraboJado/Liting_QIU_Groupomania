dcf296b77dd19cd20a2a15fcb9381869dea983a2
lq <liting_qiu@hotmail.com>
2022-09-27 14:59:36 +0200

    add headerNav

---
+++ client/src/components/Header/HeaderNav.jsx
@@ -0,0 +1,34 @@
+import React , { useState } from 'react';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars,faXmark } from '@fortawesome/free-solid-svg-icons';
+
+export default function HeaderNav(props) {
+    const [isShow, setIsShow] = useState(false);
+
+    const clickHandler = () => {
+        setIsShow(!isShow)
+    }
+
+  return (
+    <nav className='header__nav'>
+        {isShow ? 
+        <FontAwesomeIcon icon={faXmark} onClick = {clickHandler}/>
+        :
+        <FontAwesomeIcon icon={faBars} onClick = {clickHandler}/> 
+        }
+          
+        {isShow && 
+        <div className='header__dropDown'>
+            <Link to = '/signup' className='header__button--dark'>S'inscrire</Link>
+            <Link to = '/login'  className='header__button--dark' >S'identifier</Link>
+        </div>
+        }
+
+        {/* <div className='dropDown header__dropDown'>
+            <Link to = '/signup' className='header__button--dark'>S'inscrire</Link>
+            <Link to = '/login'  className='header__button--dark' >S'identifier</Link>
+        </div> */}
+    </nav>
+  )
+}
