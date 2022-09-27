fcfccc672a6890f9c65b38e1e54c2756b8fed389
lq <liting_qiu@hotmail.com>
2022-09-27 15:13:01 +0200

    add Home route

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import {
 import'./app.scss';
 import Landing from './pages/landing/Landing';
 import Signup from "./pages/signup/Signup";
-
 import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
 
 
 
@@ -17,8 +17,9 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/home" element={<Home />}></Route>
           <Route path="/" element={<Landing />}></Route>
-          <Route path="/home" element={ <div>HOME</div>}></Route>
+
         </Routes>
       </BrowserRouter>
     
