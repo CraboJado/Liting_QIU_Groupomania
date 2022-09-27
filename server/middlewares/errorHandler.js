5b231d38b5f870ef9f0703962ea51dc255b8b566
lq <liting_qiu@hotmail.com>
2022-09-27 14:25:02 +0200

    add logic to handle error of JWT

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -6,17 +6,21 @@ const errorHandler = (err, req, res, next) => {
     let error = {...err};
     error.message = err.message 
     console.log(error);
+    console.log(error.errno)
+    console.log(error.name)
 
     // if error is populated from MySQL server error, response MySQL server error symbol (error.code)
     if(error.errno) {
+        console.log('MySQL')
         error = new ErrorResponse(error.code,400);
     }
 
     // if error is from JsonWebToken, customize error response
-    if(error.name === 'JsonWebTokenError' || 'TokenExpiredError') {
+    if(error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
+        console.log('JWT')
         error = new ErrorResponse(error.message,400);
     }
-
+    console.log(error.statusCode)
     res.status(error.statusCode || 500).json({ errorMsg: error.message || "Server Error"})
 }
 
