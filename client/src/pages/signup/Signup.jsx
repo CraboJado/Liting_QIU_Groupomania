9b138dfbf2b7834895a3502dd82dca947e434652
lq <liting_qiu@hotmail.com>
2022-09-27 15:04:34 +0200

    modify Signup

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import { Link } from "react-router-dom";
-import Form from '../../components/form/Form';
+import Form from '../../components/signupForm/Form';
+import './signup.scss'
 
 export default function Signup() {
   return (
-    <div>
+    <div className='signup'>
       <Header>
-        <Link to = '/login'  className='btn' >login</Link>
+        <div className="linkWrap header__linkWrap">
+          <Link to = '/login'  className='link header__link' >S'identifier</Link>
+        </div>
       </Header>
       <Form/>
       <Footer/>
